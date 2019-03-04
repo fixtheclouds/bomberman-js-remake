c142ed6807881f8be74b5fc4625948ddcc022d0c
Egor Romanov <unsaidxpl@gmail.com>
2019-03-04 23:18:11 +0300

    Add code quality tools

--- src/Scene.js
+++ src/Scene.js
@@ -1,10 +1,6 @@
-import SoundManager from './SoundManager';
-
 export default class Scene {
-
   constructor(game) {
     this._game = game;
     this._ctx = this._game.ctx;
   }
-
 }
--- src/game/Bomb.js
+++ src/game/Bomb.js
@@ -1,24 +1,34 @@
-import Sprite from '../Sprite';
 import AnimatedSprite from '../AnimatedSprite';
 import * as constants from '../constants';
 
 export default class Bomb {
-
   constructor(scene, x, y, isDetonatable) {
     this.x = x;
     this.y = y;
     this.scene = scene;
     this.timer;
     this.isDetonatable = isDetonatable;
-    this.sprite = new AnimatedSprite('sprite.png', 83, 21, 16, 16, [0, 1, 2], [0, 1, 3]);
+    this.sprite = new AnimatedSprite(
+      'sprite.png',
+      83,
+      21,
+      16,
+      16,
+      [0, 1, 2],
+      [0, 1, 3]
+    );
     this.sprite.speed = 1;
     this.seconds = 2;
   }
 
   draw(ctx) {
-    this.sprite.animate(ctx, this.x * constants.UNIT_WIDTH, constants.MAP_TOP_MARGIN + this.y * constants.UNIT_HEIGHT, 0.2);
+    this.sprite.animate(
+      ctx,
+      this.x * constants.UNIT_WIDTH,
+      constants.MAP_TOP_MARGIN + this.y * constants.UNIT_HEIGHT,
+      0.2
+    );
   }
-
 
   destroy() {
     this.scene.blocks[this.x][this.y] = null;
@@ -50,5 +60,4 @@ export default class Bomb {
       }
     }, 1000);
   }
-
 }
